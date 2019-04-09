533e219a7d92558889ca8abb24b75b4f258342a9
dannamachi <serenita15@gmail.com>
2019-04-09 19:45:28 +1000

    Fix GameLogic.cs

--- src/GameLogic.cs
+++ src/GameLogic.cs
@@ -8,21 +8,21 @@ static class GameLogic
         SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
 
         // Load Resources
-        LoadResources();
+        GameResources.LoadResources();
 
-        SwinGame.PlayMusic(GameMusic("Background"));
+        SwinGame.PlayMusic(GameResources.GameMusic("Background"));
 
         // Game Loop
         do
         {
-            HandleUserInput();
-            DrawScreen();
+            GameController.HandleUserInput();
+            GameController.DrawScreen();
         }
-        while (!SwinGame.WindowCloseRequested() == true | CurrentState == GameState.Quitting);
+        while (!SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting);
 
         SwinGame.StopMusic();
 
         // Free Resources and Close Audio, to end the program.
-        FreeResources();
+        GameResources.FreeResources();
     }
 }
