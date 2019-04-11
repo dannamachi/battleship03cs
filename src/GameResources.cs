0364ca9072ea20a28454213685465a5b4510d246
dannamachi <serenita15@gmail.com>
2019-04-11 15:38:18 +1000

    Delete Resources/ in lib/Debug

--- src/GameResources.cs
+++ src/GameResources.cs
@@ -235,7 +235,7 @@ public static class GameResources
         SwinGame.FreeBitmap(_Animation);
         SwinGame.FreeBitmap(_LoaderEmpty);
         SwinGame.FreeBitmap(_LoaderFull);
-        Audio.FreeSoundEffect(_StartSound);
+        //Audio.FreeSoundEffect(_StartSound);
         SwinGame.ChangeScreenSize(width, height);
     }
 
